eb9f9b9bb3714927188464af2ee96d6becefc2c8
KingBob <52259912+Kingbobs@users.noreply.github.com>
2023-06-24 18:10:09 -0600

    Update Main.php

--- Kingbobs/PrisonCell/Main.php
+++ Kingbobs/PrisonCell/Main.php
@@ -2,118 +2,77 @@
 
 namespace Kingbobs;
 
+use pocketmine\block\Block;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\block\SignChangeEvent;
+use pocketmine\level\Position;
+use pocketmine\tile\Sign;
+use pocketmine\utils\Config;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\event\block\SignChangeEvent;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\utils\Config;
-use pocketmine\level\Position;
-use pocketmine\block\Block;
-use pocketmine\tile\Sign;
-use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\item\Item;
-use onebone\economyapi\EconomyAPI;
 
-class PrisonCellPlugin extends PluginBase implements Listener
+class PrisonCellPlugin extends PluginBase
 {
-    /** @var Config */
-    private $config;
-
-    /** @var array */
     private $prisonCells = [];
+    private $defaultCellPrice = 100;
+    private $messages;
 
     public function onEnable()
     {
-        $this->saveDefaultConfig();
-        $this->config = $this->getConfig();
-        $this->prisonCells = $this->config->get("prison_cells", []);
+        $this->loadConfig();
+        $this->loadMessages();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
-    public function onDisable()
+    private function loadConfig()
     {
-        $this->config->set("prison_cells", $this->prisonCells);
-        $this->config->save();
+        $this->saveDefaultConfig();
+        $this->reloadConfig();
+        $config = $this->getConfig();
+
+        // Load prison cells from config
+        $prisonCells = $config->get("prison_cells", []);
+        foreach ($prisonCells as $cell => $cellData) {
+            $x = $cellData["x"];
+            $y = $cellData["y"];
+            $z = $cellData["z"];
+            $this->prisonCells[$cell] = new Position($x, $y, $z, $this->getServer()->getDefaultLevel());
+        }
+
+        // Load default cell price from config
+        $this->defaultCellPrice = $config->get("cell_price", 100);
     }
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    private function loadMessages()
     {
-        if ($command->getName() === "setcell") {
-            if (!$sender instanceof Player) {
-                $sender->sendMessage("This command can only be run in-game.");
-                return true;
-            }
+        $this->messages = (new Config($this->getDataFolder() . "messages.yml", Config::YAML, [
+            "buy_success" => "You have bought cell '{cell}' for ${price}.",
+            "buy_insufficient_funds" => "You don't have enough money to buy cell '{cell}'.",
+            "remove_success" => "Cell '{cell}' removed successfully.",
+            "cell_not_found" => "Cell '{cell}' does not exist.",
+            "sign_header" => "§a[Buy Cell]",
+            "sign_price_format" => "§7Price: ${price}",
+            "sign_tap_to_buy" => "§7Tap to buy"
+        ]))->getAll();
+    }
 
-            // Check if the player has permission to use the command
-            if (!$sender->hasPermission("cell.set")) {
-                $sender->sendMessage("You don't have permission to use this command.");
-                return true;
-            }
+    private function saveMessages()
+    {
+        $messagesConfig = new Config($this->getDataFolder() . "messages.yml", Config::YAML);
+        $messagesConfig->setAll($this->messages);
+        $messagesConfig->save();
+    }
 
-            // Check if the command has the correct number of arguments
-            if (count($args) !== 1) {
-                $sender->sendMessage("Usage: /setcell <cell>");
-                return true;
-            }
-
-            $cell = $args[0];
-
-            // Check if the cell already exists
-            if (isset($this->prisonCells[$cell])) {
-                $sender->sendMessage("Cell '$cell' already exists.");
-                return true;
-            }
-
-            // Save the cell location to the config file
-            $this->prisonCells[$cell] = $sender->getPosition()->asVector3();
-            $this->config->set("prison_cells", $this->prisonCells);
-            $this->config->save();
-
-            $sender->sendMessage("Cell '$cell' set successfully.");
-            return true;
+    private function getMessage(string $key, array $params = []): string
+    {
+        $message = $this->messages[$key] ?? "";
+        foreach ($params as $param => $value) {
+            $message = str_replace("{" . $param . "}", $value, $message);
         }
-
-        if ($command->getName() === "removecell") {
-            if (!$sender instanceof Player) {
-                $sender->sendMessage("This command can only be run in-game.");
-                return true;
-            }
-
-            // Check if the player has permission to use the command
-            if (!$sender->hasPermission("cell.remove")) {
-                $sender->sendMessage("You don't have permission to use this command.");
-                return true;
-            }
-
-            // Check if the command has the correct number of arguments
-            if (count($args) !== 1) {
-                $sender->sendMessage("Usage: /removecell <cell>");
-                return true;
-            }
-
-            $cell = $args[0];
-
-            // Check if the cell exists
-            if (!isset($this->prisonCells[$cell])) {
-                $sender->sendMessage("Cell '$cell' does not exist.");
-                return true;
-            }
-
-            // Remove the cell from the config
-            unset($this->prisonCells[$cell]);
-            $this->config->set("prison_cells", $this->prisonCells);
-            $this->config->save();
-
-            $sender->sendMessage("Cell '$cell' removed successfully.");
-            return true;
-        }
-
-        return false;
+        return $message;
     }
 
     public function onSignChange(SignChangeEvent $event)
@@ -124,16 +83,14 @@ class PrisonCellPlugin extends PluginBase implements Listener
         }
 
         $lines = $event->getLines();
-        if ($lines[0] !== "[Buy Cell]") {
+        if ($lines[0] !== $this->getMessage("sign_header")) {
             return;
         }
 
         $cell = $lines[1];
         if (isset($this->prisonCells[$cell])) {
-            $event->setLine(0, "§a[Buy Cell]");
-            $event->setLine(1, $cell);
-            $event->setLine(2, "§7Price: $100");
-            $event->setLine(3, "§7Tap to buy");
+            $event->setLine(0, $this->getMessage("sign_header"));
+            $event->setLine(3, $this->getMessage("sign_tap_to_buy"));
         }
     }
 
@@ -145,31 +102,22 @@ class PrisonCellPlugin extends PluginBase implements Listener
         if ($block->getId() === Block::SIGN_POST || $block->getId() === Block::WALL_SIGN) {
             $tile = $player->getLevel()->getTile($block);
             if ($tile instanceof Sign) {
-                $lines = $tile->getText();
+                $text = $tile->getText();
 
-                if ($lines[0] === "§a[Buy Cell]") {
-                    $cell = $lines[1];
-                    $price = 100; // Change this to the desired cell price
+                if ($text[0] === $this->getMessage("sign_header")) {
+                    $cell = $text[1];
+                    $price = $this->defaultCellPrice;
 
                     if (isset($this->prisonCells[$cell])) {
-                        // Check if the player has enough money
                         if ($this->hasEnoughMoney($player, $price)) {
-                            // Deduct the cell price from the player's money
                             $this->deductMoney($player, $price);
-
-                            // Teleport the player to the cell location
-                            $cellLocation = $this->prisonCells[$cell];
-                            $x = $cellLocation->x;
-                            $y = $cellLocation->y;
-                            $z = $cellLocation->z;
-                            $level = $player->getLevel();
-                            $position = new Position($x, $y, $z, $level);
-                            $player->teleport($position);
-
-                            $player->sendMessage("You have bought cell '$cell' for $$price.");
+                            $this->putInCell($player, $cell);
+                            $player->sendMessage($this->getMessage("buy_success", ["cell" => $cell, "price" => $price]));
                         } else {
-                            $player->sendMessage("You don't have enough money to buy cell '$cell'.");
+                            $player->sendMessage($this->getMessage("buy_insufficient_funds", ["cell" => $cell]));
                         }
+                    } else {
+                        $player->sendMessage($this->getMessage("cell_not_found", ["cell" => $cell]));
                     }
                 }
             }
@@ -179,27 +127,31 @@ class PrisonCellPlugin extends PluginBase implements Listener
     public function onBlockBreak(BlockBreakEvent $event)
     {
         $block = $event->getBlock();
-        $tile = $event->getPlayer()->getLevel()->getTile($block);
-        
-        if ($tile instanceof Sign) {
-            $lines = $tile->getText();
 
-            if ($lines[0] === "§a[Buy Cell]") {
-                $event->setCancelled();
+        if ($block->getId() === Block::SIGN_POST || $block->getId() === Block::WALL_SIGN) {
+            $tile = $event->getPlayer()->getLevel()->getTile($block);
+            if ($tile instanceof Sign) {
+                $text = $tile->getText();
+                if ($text[0] === $this->getMessage("sign_header")) {
+                    $cell = $text[1];
+                    if (isset($this->prisonCells[$cell])) {
+                        $this->removeFromCell($cell);
+                        unset($this->prisonCells[$cell]);
+                        $event->getPlayer()->sendMessage($this->getMessage("remove_success", ["cell" => $cell]));
+                    }
+                }
             }
         }
     }
 
-    public function onPlayerQuit(PlayerQuitEvent $event)
-    {
-        $player = $event->getPlayer();
-        $this->removeFromCell($player);
-    }
-
     public function onPlayerJoin(PlayerJoinEvent $event)
     {
-        $player = $event->getPlayer();
-        $this->removeFromCell($player);
+        // Handle player join logic, if needed
+    }
+
+    public function onPlayerQuit(PlayerQuitEvent $event)
+    {
+        // Handle player quit logic, if needed
     }
 
     private function hasEnoughMoney(Player $player, int $amount): bool
@@ -212,27 +164,15 @@ class PrisonCellPlugin extends PluginBase implements Listener
         EconomyAPI::getInstance()->reduceMoney($player, $amount);
     }
 
-    private function removeFromCell(Player $player)
+    private function putInCell(Player $player, string $cell)
     {
-        // Remove the player from any cell they were in
-    }
-  private function loadConfig()
-  {
-    $this->saveDefaultConfig();
-    $this->reloadConfig();
-    $config = $this->getConfig();
-
-    // Load prison cells from config
-    $prisonCells = $config->get("prison_cells", []);
-    foreach ($prisonCells as $cell => $cellData) {
-        $x = $cellData["x"];
-        $y = $cellData["y"];
-        $z = $cellData["z"];
-        $this->prisonCells[$cell] = new Position($x, $y, $z, $this->getServer()->getDefaultLevel());
+        // Put the player in the specified cell
+        // Replace this with your prison cell handling logic
     }
 
-    // Load default cell price from config
-    $this->defaultCellPrice = $config->get("cell_price", 100);
-}
-
+    private function removeFromCell(string $cell)
+    {
+        // Remove the specified cell
+        // Replace this with your prison cell handling logic
+    }
 }
